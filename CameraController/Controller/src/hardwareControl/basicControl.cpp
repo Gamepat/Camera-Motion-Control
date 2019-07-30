c68c3ec96bd2da79786fd800e640d5ce24945af1
Gamepat <phyrofreaky@gmail.com>
2019-07-30 17:30:14 +0200

    Added basic pin control for focus and trigger

---
+++ CameraController/Controller/src/hardwareControl/basicControl.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define FOCUS_PIN 4
+#define TRIGGER_PIN 5
+
+
+
+void initPins() {
+  pinMode(FOCUS_PIN, OUTPUT);
+  pinMode(TRIGGER_PIN, OUTPUT);
+}
+
+
+void trigger() {
+
+
+}
+
+
+void focus() {
+
+
+}
